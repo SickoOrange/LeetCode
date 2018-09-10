40c2e478dad23639af3e5f59a2105917d2c78130
Ya Yin <181536584@qq.com>
2018-09-10 21:13:46 +0200

    Create 709_ToLowerCase.java

---
+++ 709_ToLowerCase.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String toLowerCase(String str) {
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c >= 'A' && c <= 'Z') {
+                chars[i] += 32;
+            }
+        }
+        return new String(chars);
+    }
+}
