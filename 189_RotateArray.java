06f76e6c6684362bfe09b4f9d592be1da4eca692
Ya Yin <181536584@qq.com>
2018-09-10 20:53:19 +0200

    Update 189_RotateArray.java

--- 189_RotateArray.java
+++ 189_RotateArray.java
@@ -18,3 +18,23 @@ class Solution {
     }
   }
 }
+
+//ac
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int length = nums.length;
+        int count=0;
+        for (int i = 0; count < length; i++) {
+            int index = i;
+            int prev = nums[i];
+            do {
+                index = (index + k) % length;
+                int next = nums[index];
+                nums[index] = prev;
+                prev = next;
+                count++;
+            } while (index != i);
+        }
+    }
+}
