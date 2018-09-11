46b3b30530b6e6ab2d5be771c18f94858b344f2e
Ya Yin <181536584@qq.com>
2018-09-11 21:59:39 +0200

    Create 344_ReverseString.java

---
+++ 344_ReverseString.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String reverseString(String s) {
+         char[] chars = s.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+
+        while (end > start) {
+            char temp = chars[end];
+            chars[end] = chars[start];
+            chars[start] = temp;
+            start++;
+            end--;
+        }
+        return new String(chars);
+    }
+}
