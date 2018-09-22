8b3205757bd9e14415ebb57f8f02f219b4073462
Ya Yin <181536584@qq.com>
2018-09-22 22:54:42 +0100

    Update 14_LongestCommonPrefix.java

--- 14_LongestCommonPrefix.java
+++ 14_LongestCommonPrefix.java
@@ -36,3 +36,37 @@ class Solution {
         return commonPrefix;
     }
 }
+
+//divide and conquer
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+
+        if (strs.length == 0) return "";
+        if (strs.length == 1) return strs[0];
+
+        return divideAndConquer(strs, 0, strs.length - 1);
+    }
+
+    private String divideAndConquer(String[] strs, int l, int r) {
+
+        if (l == r) return strs[l];
+
+        int mid = (l + r) / 2;
+
+        String left = divideAndConquer(strs, l, mid);
+        String right = divideAndConquer(strs, mid + 1, r);
+
+        while (left.indexOf(right) != 0) {
+            right=right.substring(0, right.length() - 1);
+        }
+        return right;
+       
+        //int min = Math.min(left.length(), right.length());       
+        //for (int i = 0; i < min; i++) {
+        //if ( left.charAt(i) != right.charAt(i) )
+        //    return left.substring(0, i);
+        //}
+        //return left.substring(0, min);
+        
+    }
+}
