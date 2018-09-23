fe5427695bc8f2a3840aa64e648aa695eca78085
Ya Yin <181536584@qq.com>
2018-09-23 13:14:19 +0100

    Update 876_MiddleoftheLinkedList.java

--- 876_MiddleoftheLinkedList.java
+++ 876_MiddleoftheLinkedList.java
@@ -23,3 +23,15 @@ class Solution {
         return singlePoint;
     }
 }
+
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
