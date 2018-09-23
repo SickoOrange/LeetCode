ff968311d6ac6de314cedde224f8066235d5c052
Ya Yin <181536584@qq.com>
2018-09-23 13:11:39 +0100

    Create 876_MiddleoftheLinkedList.java

---
+++ 876_MiddleoftheLinkedList.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode doublePoint = head;
+        ListNode singlePoint = head;
+
+        while (doublePoint != null && doublePoint.next != null && doublePoint.next.next != null) {
+            doublePoint = doublePoint.next.next;
+            singlePoint = singlePoint.next;
+        }
+
+        if (doublePoint.next != null && doublePoint.next.next == null) {
+            //there are two middle nodes in the list
+            singlePoint = singlePoint.next;
+        }
+        return singlePoint;
+    }
+}
