78ed7d248e071dad7b631a402bfea0d43a46b483
Ya Yin <yayin@YadeMacBook-Pro.fritz.box>
2018-09-25 19:00:16 +0200

    21,237 two solutuion

---
+++ 21_MergeTwoSortedLists.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+
+// iterator solution
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+
+        ListNode node = new ListNode(-1);
+        ListNode curr = node;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+
+        }
+        curr.next = l1 == null ? l2 : l1;
+        return node.next;
+    }
+}
+
+// recursive mnethod solution
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
+    }
+}
